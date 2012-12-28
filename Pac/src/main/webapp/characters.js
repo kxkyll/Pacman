287b622c6d21bc00b7e41abb63c90c6416a809f3
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2012-12-28 22:54:09 +0200

    Pacman moving is not yet working

--- Pac/src/main/webapp/characters.js
+++ Pac/src/main/webapp/characters.js
@@ -3,7 +3,7 @@ function Pacman (x,y,r){
     this.y=y;
     this.r=r;
     this.colour = "rgb(255,255,0)";
-    console.log("pacman luotu");
+//console.log("pacman luotu");
 }
 
 Pacman.prototype.getX = function(){
@@ -15,20 +15,51 @@ Pacman.prototype.getY = function(){
 Pacman.prototype.getR = function(){
     return this.r;
 }
+//Pacman.prototype.setX = function(x){
+//    this.x = x;
+//}
+//Pacman.prototype.setY = function(y){
+//    this.y = y;
+//}
+
+Pacman.prototype.move = function(xAndy){
+    var x=xAndy[0];   
+    var y=xAndy[1];
+    if (x === 0 && y === -1){ //up
+        this.y -= y;
+        return;
+    }
+    if (x === 0 && y === 1){ //down
+        this.y += y;
+        return;
+    }
+    if (x === -1 && y === 0){ //left
+        this.x -= x; 
+        return;
+    }
+    if (x === 1 && y === 0){ //right
+        this.x += x;
+        return;
+    }
+    
+    
+    
+}
+
+
 
 Pacman.prototype.changeColour = function(colour){
     this.colour = colour;
 }
 
 Pacman.prototype.draw = function(context){
-    console.log("pacmania piirtämässä");    
+    //console.log("pacmania piirtämässä");    
     context.beginPath();
-    
     context.arc(this.x, this.y, this.r, 0, 2* Math.PI, false);
     context.fillStyle=this.colour;
     context.fill();
-    context.lineWidth =2;
-    context.strokeStyle = '#003300';
+    //context.lineWidth =2;
+    //context.strokeStyle = '#003300';
     context.stroke();
 
 }
--- Pac/src/main/webapp/game.js
+++ Pac/src/main/webapp/game.js
@@ -1,8 +1,48 @@
+"use strict";
+window.requestAnimFrame = (function(){
+    return window.requestAnimationFrame       || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame    || 
+    window.oRequestAnimationFrame      || 
+    window.msRequestAnimationFrame     || 
+    function(callback, element){
+        window.setTimeout(callback, 1000 / 60); //1000/60
+    };
+})();
+
+
 $(document).ready(function(){
     console.log("document ready");
-    var context = $("#pacman")[0].getContext("2d");
-    context.fillStyle="rgb(105,105,105)";
-    context.fillRect(0,0, 640,480);
-    var pac = new Pacman (120, 100, 20);
-    pac.draw(context);
+    pacmanGame.init();
+    $(document).keyup(function(eventInfo) {
+        console.log(eventInfo.which);
+        keyhandler.keyup(eventInfo.which);
+    });
+        
+    $(document).keydown(function(eventInfo) {
+        console.log(eventInfo.which);
+        keyhandler.keydown(eventInfo.which);
+    });
 })
+
+var pacmanGame ={
+    ctx: null,
+    man: null,
+    gameOver: null,
+    init: function(){
+        console.log("init");
+        pacmanGame.ctx = $("#pacman")[0].getContext("2d");
+        pacmanGame.ctx.fillStyle="rgb(105,105,105)";
+        pacmanGame.ctx.fillRect(0,0, 640,480);
+        pacmanGame.man = new Pacman (120, 100, 20);
+        pacmanGame.man.draw(pacmanGame.ctx);
+        pacmanGame.run();
+    },
+    run: function(){
+        //console.log("run");
+        pacmanGame.man.move(keyhandler.getMovement());
+        pacmanGame.man.draw(pacmanGame.ctx);
+        requestAnimFrame(pacmanGame.run());
+        //setTimeout(pacmanGame.run(),1000);
+    }
+};
